3f211344be20e9da9401593f2cf8c8022cde2e5f
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-01 13:41:10 +0100

    add enum class to Samples.dart

--- workings/Samples.dart
+++ workings/Samples.dart
@@ -2,7 +2,7 @@ var name = 'Damola Olutoba Onikoyi';
 var year = 2025;
 var age = 15;
 var hasGraduated = true;
-var hobbies = ['Swimming', 'Football', 'Movies', 'Reading'];
+var hobbies = ['Swimming', 'Football', 'Movies', 'Reading', 'Coding'];
 var places = {
   'city': ['San Francisco'],
   'country': ['USA'],
@@ -18,6 +18,8 @@ void main() {
     print('21st century');
   } else if (year >= 1901) {
     print('20th century');
+  } else {
+    print('Unknown century');
   }
 
   for (final currHobbies in hobbies) {
@@ -43,4 +45,32 @@ int fibonacci(int n) {
   }
 
   return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+enum UserRole { admin, user, guest }
+
+enum PlanetType { terrestrial, gas, ice }
+
+void checkAccess(UserRole role) {
+  if (role == UserRole.admin) {
+    print('give full access');
+  } else if (role == UserRole.user) {
+    print('give partial access');
+  } else {
+    print('no access');
+  }
+}
+
+void printRole(UserRole role) {
+  switch (role) {
+    case UserRole.admin:
+      print('Admin privileges');
+      break;
+    case UserRole.user:
+      print('User privileges');
+      break;
+    case UserRole.guest:
+      print('No privileges');
+      break;
+  }
 }
