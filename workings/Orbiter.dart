f9d5ccd13311da2ecaf69a2a450983162679df41
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-01 14:15:41 +0100

    add mixins sample

--- workings/Orbiter.dart
+++ workings/Orbiter.dart
@@ -1,5 +1,19 @@
 import 'Spacecraft.dart';
 
 class Orbiter extends Spacecraft {
-  Orbiter(super.name, super.launchDate);
+  double altitude;
+
+  Orbiter(super.name, super.launchDate, this.altitude);
+}
+
+mixin Piloted {
+  int astronauts = 1;
+
+  void describeCrew() {
+    print('Number of astronauts: $astronauts');
+  }
+}
+
+class PilotedCraft extends Spacecraft with Piloted {
+  PilotedCraft(super.name, super.launchDate);
 }
